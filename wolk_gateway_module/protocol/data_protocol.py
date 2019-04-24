d626e89cf1fedfd721318a1bf07c23cdbb666ee6
djesic <danilo.jesic@wolkabout.com>
2019-04-24 16:24:58 +0200

    Data protocol ABC

---
+++ wolk_gateway_module/protocol/data_protocol.py
@@ -0,0 +1,148 @@
+"""Handling of outbound and inbound messages related to device data."""
+#   Copyright 2019 WolkAbout Technology s.r.o.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from abc import ABC, abstractmethod
+
+
+class DataProtocol(ABC):
+    """Parse inbound messages and serialize outbound messages."""
+
+    @abstractmethod
+    def is_actuator_get_message(self, message):
+        """Check if message is actuator get command.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: is_actuator_get_message
+        :rtype: bool
+        """
+        pass
+
+    @abstractmethod
+    def is_actuator_set_message(self, message):
+        """Check if message is actuator set command.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: is_actuator_set_message
+        :rtype: bool
+        """
+        pass
+
+    @abstractmethod
+    def is_configuration_get_message(self, message):
+        """Check if message is configuration get command.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: is_configuration_get_message
+        :rtype: bool
+        """
+        pass
+
+    @abstractmethod
+    def is_configuration_set_message(self, message):
+        """Check if message is configuration set command.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: is_configuration_set_message
+        :rtype: bool
+        """
+        pass
+
+    @abstractmethod
+    def make_actuator_command(self, message):
+        """Make actuator command from message.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: actuator_command
+        :rtype: wolk_gateway_module.model.actuator_command.ActuatorCommand
+        """
+        pass
+
+    @abstractmethod
+    def make_configuration_command(self, message):
+        """Make configuration command from message.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: configuration_command
+        :rtype: wolk_gateway_module.model.configuration_command.ConfigurationCommand
+        """
+        pass
+
+    @abstractmethod
+    def make_sensor_reading_message(self, device_key, sensor_reading):
+        """Make message from sensor reading for device key.
+
+        :param device_key: Device on which the sensor reading occurred
+        :type device_key: str
+        :param sensor_reading: Sensor reading data
+        :type sensor_reading: wolk_gateway_module.model.sensor_reading.SensorReading
+
+        :returns: message
+        :rtype: wolk_gateway_module.model.message.Message
+        """
+        pass
+
+    @abstractmethod
+    def make_alarm_message(self, device_key, alarm):
+        """Make message from alarm for device key.
+
+        :param device_key: Device on which the alarm occurred
+        :type device_key: str
+        :param alarm: Alarm data
+        :type alarm: wolk_gateway_module.model.alarm.Alarm
+
+        :returns: message
+        :rtype: wolk_gateway_module.model.message.Message
+        """
+        pass
+
+    @abstractmethod
+    def make_actuator_status_message(self, device_key, actuator_status):
+        """Make message from actuator status for device key.
+
+        :param device_key: Device on which the actuator status occurred
+        :type device_key: str
+        :param actuator_status: Actuator status data
+        :type actuator_status: wolk_gateway_module.model.actuator_status.ActuatorStatus
+
+        :returns: message
+        :rtype: wolk_gateway_module.model.message.Message
+        """
+        pass
+
+    @abstractmethod
+    def make_configuration_message(self, device_key, configuration):
+        """Make message from configuration for device key.
+
+        :param device_key: Device to which the configuration belongs to.
+        :type device_key: str
+        :param configuration: Current configuration data
+        :type configuration: dict
+
+        :returns: message
+        :rtype: wolk_gateway_module.model.message.Message
+        """
+        pass
