f64fa63744038399c51a98146d5c2d444a9e164d
djesic <danilo.jesic@wolkabout.com>
2019-04-24 16:41:32 +0200

    Registration protocol ABC

---
+++ wolk_gateway_module/protocol/registration_protocol.py
@@ -0,0 +1,58 @@
+"""Handling of inbound and outbound messages related to device registraiton."""
+#   Copyright 2019 WolkAbout Technology s.r.o.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from abc import ABC, abstractmethod
+
+
+class RegistrationProtocol(ABC):
+    """Parse inbound messages and serialize outbound registration messages."""
+
+    @abstractmethod
+    def is_device_registration_response_message(self, message):
+        """Check if message is device registration response.
+
+        :param message: Message received
+        :type message: wolk_gateway_module.model.message.Message
+
+        :returns: is_device_registration_response
+        :rtype: bool
+        """
+        pass
+
+    @abstractmethod
+    def make_device_registration_request_message(
+        self, device_registration_request
+    ):
+        """Make message from device registration request.
+
+        :param device_registration_request: Device registration request
+        :type device_registration_request: wolk_gateway_module.model.device_registration_request.DeviceRegistrationRequest
+
+        :returns: message
+        :rtype: wolk_gateway_module.model.message.Message
+        """
+        pass
+
+    @abstractmethod
+    def make_device_registration_response(self, message):
+        """Make device registration response from message.
+
+        :param message: Message received
+        :rtpe message: wolk_gateway_module.model.message.Message
+
+        :returns: device_registration_response
+        :rtype: wolk_gateway_module.model.device_registration_response.DeviceRegistrationResponse
+        """
+        pass
