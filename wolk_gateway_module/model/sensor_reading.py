8e7631936357173b5ed9b9a347cc1683f15e43e4
djesic <danilo.jesic@wolkabout.com>
2019-04-24 11:46:08 +0200

    Removed trailing space

---
+++ wolk_gateway_module/model/sensor_reading.py
@@ -0,0 +1,34 @@
+"""Sensor reading model."""
+#   Copyright 2019 WolkAbout Technology s.r.o.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from dataclasses import dataclass, field
+from typing import Union, Tuple
+
+
+@dataclass
+class SensorReading:
+    """Holds information about a sensor reading.
+
+    :ivar reference: Device sensor's reference as defined in device template
+    :vartype reference: str
+    :ivar value: Data that the sensor reading yielded
+    :vartype value: int or float or str or tuple
+    :ivar timestamp:  Unix timestamp in miliseconds. If not provided, Platform will assign timestamp when it receives it.
+    :vartype timestamp: int or None
+    """
+
+    reference: str
+    value: Union[int, float, str, Tuple[int, float, str]]
+    timestamp: int = field(default=None)
