99e6118efccbcf5bf78780faec1db3cdf19058aa
Danilo Ješić <danilo.jesic@wolkabout.com>
2019-06-04 11:42:37 +0200

    Changed development status to production

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=("test",)),
     test_suite="unittest",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
