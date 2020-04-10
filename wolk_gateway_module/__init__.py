6ef4da573d351ea8c5fda4cf63336479fbe05481
djesic <danilo.jesic@wolkabout.com>
2020-04-10 16:39:18 +0200

    Version bump

--- wolk_gateway_module/__init__.py
+++ wolk_gateway_module/__init__.py
@@ -1,3 +1,19 @@
+"""Module that provides connectivity for non-IP enabled devices to WolkAbout IoT Platform."""
+#   Copyright 2020 WolkAbout Technology s.r.o.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+__version__ = "1.0.7"
+
 from .connectivity.connectivity_service import ConnectivityService
 from .interface.actuation_handler import handle_actuation
 from .interface.actuator_status_provider import get_actuator_status
@@ -74,4 +90,5 @@ __all__ = [
     "DeviceRegistrationRequest",
     "DeviceRegistrationResponse",
     "DeviceStatus",
+    "__version__",
 ]
